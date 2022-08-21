b387428c441306cfa9a32d80fdbb809086a3c7fc
shilpadey <shilpadey735@gmail.com>
2022-08-21 19:29:32 +0530

    currying

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 
-var obj = {num : 5};
+//var obj = {num : 5};
 
-function addToThis(a,b,c){
-    return this.num + a + b + c;
-}
+//function addToThis(a,b,c){
+  //  return this.num + a + b + c;
+//}
 
 //console.log(addToThis.call(obj,4));
     
-  var arr = [1,5,6];
+  //var arr = [1,5,6];
 //console.log(addToThis.apply(obj,arr));  
     
-var bound = addToThis.bind(obj);
-console.log(bound(1,2,4));
+//var bound = addToThis.bind(obj);
+//console.log(bound(1,2,4));
 
-var student = {age : 20};
+//var student = {age : 20};
 
-function printAge(){
-    return this.age;
+//function printAge(){
+  //  return this.age;
+//}
+
+//var bound1 = printAge.bind(student);
+//console.log(bound1());
+
+//let multiply = function(x,y){
+  //console.log(x*y);
+//}
+
+let multiply = function(x){
+  return function (y) {
+    console.log(x*y);
+  }
 }
 
-var bound1 = printAge.bind(student);
-console.log(bound1());
+let multiplybyTwo = multiply(2);
+multiplybyTwo(5);
 
-
+let multiplybyThree = multiply(3);
+multiplybyThree(10);
 
