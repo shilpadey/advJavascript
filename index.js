97d6c490751b7216499c7ed1a3c28d8b4f67deb1
shilpadey <shilpadey735@gmail.com>
2022-08-29 11:37:13 +0530

    placementseligibility

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 
 class Student{
-    static count = 0;
+    
     constructor(name , age , ph_number, boardmarks){
         this.name = name;
         this.age = age;
         this.phoneNumber = ph_number;
         this.marks = boardmarks;
-        Student.count += 1;
+        
     }
 
-    numberOfStudent(){
-        console.log("Number of student registered "+ Student.count);
-    }
-
-    studentEligibity(){
-        if(this.marks >= 40){
-            console.log(`${this.name} is eligible`);
-          }
-          else{
-            console.log(`${this.name} is not eligible`);
-          }
+    eligibleforPlacements(minMarks){
+        return (minAge) => {
+            if(this.marks >= minMarks && this.age > minAge){
+                console.log(this.name + " is eligible for placements");
+            }
+            else{
+                console.log(this.name + " is not eligible for placements");
+            }
+        }
     }
 }
 
 let suman = new Student('suman',20,22896,50);
-let shreya = new Student('shreya',20,22456,76);
+let shreya = new Student('shreya',18,22456,76);
 let suraj = new Student('suraj',17,22723,30);
 let saurav = new Student('saurav',21,22045,66);
 let sahar = new Student('sahar',19,22876,89);
 
-suman.studentEligibity();
-shreya.studentEligibity();
-suraj.studentEligibity();
-saurav.studentEligibity();
-sahar.studentEligibity();
+suman.eligibleforPlacements(40)(18);
+shreya.eligibleforPlacements(40)(18);
+suraj.eligibleforPlacements(40)(18);
+saurav.eligibleforPlacements(40)(18);
+sahar.eligibleforPlacements(40)(18);
 
-suman.numberOfStudent();
+
